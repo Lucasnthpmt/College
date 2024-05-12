f197f7c01b5d0cc6a810fd66f700f6f07e12f6b3
Lucas Nathã M P de Souza <79528166+Lucasnthpmt@users.noreply.github.com>
2024-05-12 11:17:58 -0300

    animal.pl

---
+++ PLP/Prolog/animal.pl
@@ -0,0 +1,42 @@
+% animal.pl
+%
+% Prof. Ausberto S. Castro Vera
+% Disciplina : Paradigmas de Ling. de Programacao
+% UENF-CCT-LCMAT-CC
+% Ano: 2024
+% Aluno: Lucas Nathã Monteiro Pimentel de Souza
+%-------------------------------------------------
+%
+%% -------------- FATOS --------------------------
+cao(fido).
+cao(duke).
+cao(rover).
+
+gato(tom).
+gato(bill).
+gato(mima).
+
+cao_grande(fido).
+cao_pequeno(rover).
+
+gato_grande(bill).
+gato_pequeno(mima).
+
+%%---------------- REGRAS --------------------------
+animal_grande(X):- cao(X),cao_grande(X).
+animal_grande(Y):- gato(Y), gato_grande(Y).
+
+animal_pequeno(A) :- cao(A), cao_pequeno(A).
+animal_pequeno(B) :- gato(B), gato_pequeno(B).
+
+persegue(X,Y) :- cao(X),gato(Y),
+                 write(X),write(' persegue a '),write(Y),nl.
+
+%%--------  consultas -------------------------------
+% cao(X).
+% gato(Y).
+% listing(cao).
+% listing(gato).
+% animal_grande(Y).
+% animal_pequeno(B).
+% persegue(M,N)
