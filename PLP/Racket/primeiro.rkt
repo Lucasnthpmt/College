0f34da97e6c43a48b34aaed64a0f0515158569d2
Lucas Nathã M P de Souza <79528166+Lucasnthpmt@users.noreply.github.com>
2024-05-12 11:45:13 -0300

    primeiro.rkt

---
+++ PLP/Racket/primeiro.rkt
@@ -0,0 +1,20 @@
+;; Introdução à Linguagem Racket (Scheme)
+;; Prof. Ausberto S. Castro Vera       (ascv@uenf.br)
+;; UENF-CCT-LCMAT - Curso de Ciencia da Computacao
+;; 2024
+;; Aluno: Lucas Nathã Monteiro Pimentel de Souza
+;; 
+;; Liguagem Advanced Student       ;; define a linguagem default
+;; O primeiro programa Racket
+;; ---------------------------------------------------
+(begin
+  (newline) ; nova linha
+  (display "Bom dia, UENF 30 anos, Nota 5 MEC 2024") ; imprimir na tela a msg entre aspas
+  (newline) ; nova linha
+  (display "Bem vindo a Linguagem Racket-Scheme! ") ;<------ mostrar na tela
+  (newline) ; nova linha
+  (display "Sou o aluno Lucas Nathã Monteiro Pimentel de Souza") ; imprimir na tela a msg entre aspas
+  (newline)
+  (display "Prática 01 - Linguagem Racket, Lucas Nathã Monteiro Pimentel de Souza, 18/04/2024")
+ )
+
